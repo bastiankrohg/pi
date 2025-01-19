3a6bb58d84004c877e4b814c06d92c829e2056cf
Bastian Krohg <98895859+bastiankrohg@users.noreply.github.com>
2025-01-19 17:35:47 +0100

    Added states

--- components/state.py
+++ components/state.py
@@ -2,6 +2,12 @@ class State:
     def __init__(self):
         self.current_state = "IDLE"
 
-    def switch_state(self, new_state):
-        # Example states: "IDLE", "RANDOM_WALK", "EXPLORATION", "PURSUIT"
-        self.current_state = new_state
+    def get_state(self):
+        return self.current_state
+
+    def set_state(self, new_state):
+        if new_state in ["IDLE", "RANDOM_WALK", "EXPLORATION", "PURSUIT"]:
+            print(f"Switching from {self.current_state} to {new_state}")
+            self.current_state = new_state
+        else:
+            raise ValueError(f"Invalid state: {new_state}")
